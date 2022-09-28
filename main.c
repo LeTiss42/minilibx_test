9e5d44721841db05bc6c6d336fc3130015c3ca57
Mathis <ravera.mathis@gmail.com>
2022-09-29 00:04:25 +0200

    first pixel

--- main.c
+++ main.c
@@ -6,15 +6,40 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:16:19 by mravera           #+#    #+#             */
-/*   Updated: 2022/09/28 16:18:13 by mravera          ###   ########.fr       */
+/*   Updated: 2022/09/29 00:02:25 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	img.img = mlx_new_image(mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
